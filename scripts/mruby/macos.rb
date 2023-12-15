7df027c27aa480bf940df9c78e35033e78931418
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-12-15 12:42:52 -0300

    Use MacOS MRuby config

---
+++ scripts/mruby/macos.rb
@@ -0,0 +1,13 @@
+MRuby::Build.new do |conf|
+  conf.toolchain
+
+  # Include gems
+  conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
+
+  # C compiler settings
+  conf.cc.defines << %w[MRB_INT64]
+  conf.cc.flags << '-fpermissive' # Needed for mruby-require
+
+  conf.enable_debug
+  conf.enable_cxx_abi
+end
