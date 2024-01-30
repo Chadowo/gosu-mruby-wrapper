450a45107b241a16cc74d4a5d8d9a84b04127fee
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-01-30 20:08:55 -0300

    Move prototypes and `#define`s to the header

--- include/fused.h
+++ include/fused.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define NOTFOUND_ERROR_MESSAGE "cannot load such file -- %s"
+#define INVALIDPATH_ERROR_MESSAGE "path cannot have \".\" or \"..\" when in fused mode"
+#define E_LOAD_ERROR (mrb_class_get(mrb, "LoadError"))
+
 bool fileAlreadyLoaded(mrb_state* mrb, mrb_value path, mrb_value loadedFeatures);
 void loadFusedRubyFile(mrb_state* mrb, mrb_value path, bool bytecode);
 void initFused(mrb_state* mrb);
--- src/fused.c
+++ src/fused.c
@@ -12,13 +12,7 @@
 
 #include <physfs.h>
 
-#define NOTFOUND_ERROR_MESSAGE "cannot load such file -- %s"
-#define INVALIDPATH_ERROR_MESSAGE "path cannot have \".\" or \"..\" when in fused mode"
-#define E_LOAD_ERROR (mrb_class_get(mrb, "LoadError"))
-
-bool fileAlreadyLoaded(mrb_state* mrb, mrb_value path, mrb_value loadedFeatures);
-void loadFusedRubyFile(mrb_state* mrb, mrb_value path, bool bytecode);
-void initFused(mrb_state* mrb);
+#include <fused.h>
 
 static bool invalidPath(const char* path) {
     // TODO: check the whole path, not only the beginning
