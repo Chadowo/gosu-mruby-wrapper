2b3446b9db29baeca9a329c4097cceee2317079f
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-02-05 23:47:31 -0300

    Lemme check this exact change triggered the error

--- src/main.c
+++ src/main.c
@@ -57,29 +57,7 @@ static void loadRubyFile(mrb_state* mrb, char* fileName, FILE* fp) {
     mrbc_context_free(mrb, cxt);
 }
 
-// Returns true if loaded correctly
-static bool loadBootFile(mrb_state* mrb, int argc, char* argv[]) {
-    char cwd[PATH_MAX];
-    char fileName[] = "boot.rb";
-    if(getcwd(cwd, sizeof(cwd)) != NULL) {
-        // Try to open and execute the file
-        char fileSeparator[2] = {FILE_SEPARATOR, '\0'}; // Make a string out of the char
-        strncat(cwd, fileSeparator, 1);
-        strncat(cwd, fileName, sizeof(cwd) - strlen(cwd) - 1);
-
-        FILE* entryPoint = fopen(cwd, "r");
-        if(entryPoint != NULL) {
-            forwardArguments(mrb, argc, argv, 0);
-            loadRubyFile(mrb, fileName, entryPoint);
-            fclose(entryPoint);
-            return true;
-        }
-    }
-
-    return false;
-}
-
-static int loadGame(mrb_state* mrb, char* path, int argc, char* argv[]) {
+static void loadGame(mrb_state* mrb, char* path, int argc, char* argv[]) {
     // FIXME: Currently PhysFS will return a PHYSFS_ERR_NOT_FOUND error when
     //        mounting the executable as it is, however the zip file at the
     //        end will be correctly mounted, I'm not sure why that's it
@@ -87,49 +65,57 @@ static int loadGame(mrb_state* mrb, char* path, int argc, char* argv[]) {
     //        manually writing a PhysFS_Io that points to the zip at the end
     //        should work, see https://icculus.org/physfs/docs/html/structPHYSFS__Io.html
 
-    // Try to load a fused archive firstly
     if(isFused(path)) {
+        // Load the Ruby code with PhysFS
         if(!PHYSFS_exists("main.rb")) {
             fprintf(stderr, "There's no main.rb in the fused archive!\n");
-            return 1;
+            return;
         }
 
-        // Load the Ruby code with PhysFS
         forwardArguments(mrb, argc, argv, 0);
         initFused(mrb);
         loadFusedRubyFile(mrb, mrb_str_new_cstr(mrb, "main.rb"), false);
-        return 0;
-    }
+    } else {
+        // Load entrypoint file in the current directory
+        char cwd[PATH_MAX];
+        char fileName[] = "boot.rb";
+        if(getcwd(cwd, sizeof(cwd)) != NULL) {
+            // Try to open and execute the file
+            char fileSeparator[2] = {FILE_SEPARATOR, '\0'}; // Make a string out of the char
+            strncat(cwd, fileSeparator, 1);
+            strncat(cwd, fileName, sizeof(cwd) - strlen(cwd) - 1);
 
-    // Try to load boot file in the current directory
-    if (loadBootFile(mrb, argc, argv)) {
-        return 0;
-    }
+            FILE* entryPoint = fopen(cwd, "r");
+            if(entryPoint != NULL) {
+                forwardArguments(mrb, argc, argv, 0);
+                loadRubyFile(mrb, fileName, entryPoint);
+                fclose(entryPoint);
+            } else {
+                // Fallback to command line input
+                if(argc > 1) {
+                    FILE* inputFile = fopen(argv[1], "r");
+                    if(inputFile == NULL) {
+                        fprintf(stderr, "Path %s is invalid!\n", argv[1]);
+                        return;
+                    }
+                    // Skip an argument since it is the file path
+                    forwardArguments(mrb, argc, argv, 1);
+                    loadRubyFile(mrb, argv[1], inputFile);
 
-    // Fallback to command line input
-    if(argc > 1) {
-        FILE* inputFile = fopen(argv[1], "r");
-        if(inputFile == NULL) {
-            fprintf(stderr, "Path %s is invalid!\n", argv[1]);
-            return 1;
+                    fclose(inputFile);
+                } else {
+                    // Print info and usage
+                    printf("Version: %d.%d.%d (%s)\n",
+                           VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH,
+                           GIT_HASH);
+                    printf("Usage: %s [inputfile]\n", path);
+                    printf("\n");
+                    printf("[inputfile]\tEither a .rb or .mrb file\n");
+                    return;
+                }
+            }
         }
-
-        // Skip an argument since it's the file path
-        forwardArguments(mrb, argc, argv, 1);
-        loadRubyFile(mrb, argv[1], inputFile);
-        fclose(inputFile);
-        return 0;
     }
-
-
-    // If all the above fails, print info and usage
-    printf("Version: %d.%d.%d (%s)\n",
-           VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH,
-           GIT_HASH);
-    printf("Usage: %s [inputfile]\n", path);
-    printf("\n");
-    printf("[inputfile]\tEither a .rb or .mrb file\n");
-    return 0;
 }
 
 /* Remove the executable from the absolute path */
@@ -179,12 +165,11 @@ int main(int argc, char* argv[]) {
 
     if(mrb->exc) {
         mrb_print_error(mrb);
-        exitCode = 1;
     }
 
     free(dirPath);
     free(path);
     PHYSFS_deinit();
     mrb_close(mrb);
-    return exitCode;
+    return 0;
 }
