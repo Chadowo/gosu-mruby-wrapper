71bf3dc3d4c15d7b8ff5a14bbea998df4869c68c
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-03-07 10:50:16 -0300

    Disable debugging and enable optimizations

--- scripts/mruby/linux.rb
+++ scripts/mruby/linux.rb
@@ -5,9 +5,8 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines << %w[MRB_INT64]
-  conf.cc.flags << '-fpermissive' # Needed for mruby-require
+  conf.cc.defines += %w[MRB_INT64]
+  conf.cc.flags += %w[-fpermissive -O3]
 
-  conf.enable_debug
   conf.enable_cxx_abi
 end
--- scripts/mruby/windows.rb
+++ scripts/mruby/windows.rb
@@ -5,11 +5,10 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines << %w[MRB_INT64 _WIN64]
-  conf.cc.flags << '-fpermissive' # Needed for mruby-require
+  conf.cc.defines += %w[MRB_INT64 _WIN64]
+  conf.cc.flags += %w[-fpermissive -O3]
 
   linker.libraries.push('ws2_32')
 
-  conf.enable_debug
   conf.enable_cxx_abi
 end
