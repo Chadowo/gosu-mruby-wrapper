715fbeeb88edf5f8545b8b875e23f57ce3cdf193
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-03-07 11:13:27 -0300

    Let's revert the Windows MRuby config to see if that was the cause

--- scripts/mruby/windows.rb
+++ scripts/mruby/windows.rb
@@ -5,10 +5,11 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines += %w[MRB_INT64]
-  conf.cc.flags += %w[-fpermissive -O3]
+  conf.cc.defines << %w[MRB_INT64 _WIN64]
+  conf.cc.flags << '-fpermissive'
 
   linker.libraries.push('ws2_32')
 
+  conf.enable_debug
   conf.enable_cxx_abi
 end
