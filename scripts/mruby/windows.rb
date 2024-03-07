94f8109e4b761bbbb884ac07ec54546b20f59307
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-03-07 11:27:23 -0300

    Welp, looks like it

--- scripts/mruby/windows.rb
+++ scripts/mruby/windows.rb
@@ -5,10 +5,11 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines << %w[MRB_INT64 _WIN64]
-  conf.cc.flags << '-fpermissive'
+  conf.cc.defines += %w[MRB_INT64]
+  conf.cc.flags += %w[-fpermissive -O3]
 
   linker.libraries.push('ws2_32')
 
+  conf.enable_debug # Won't compile otherwise
   conf.enable_cxx_abi
 end
