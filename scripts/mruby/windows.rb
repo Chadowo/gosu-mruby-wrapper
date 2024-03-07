7b7b91faf7fd0e8fc99ff192d0b44da9573b9b2b
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-03-07 11:04:34 -0300

    Try to fix Windows compilation

--- scripts/mruby/windows.rb
+++ scripts/mruby/windows.rb
@@ -5,7 +5,7 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines += %w[MRB_INT64 _WIN64]
+  conf.cc.defines += %w[MRB_INT64]
   conf.cc.flags += %w[-fpermissive -O3]
 
   linker.libraries.push('ws2_32')
