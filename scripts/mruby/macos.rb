713cd500a27171ee01390d612f803ab794c8d085
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-09-02 18:29:41 -0300

    Use `conf.defines` instead of the C compiler defines (as recommended by MRuby)

--- scripts/mruby/macos.rb
+++ scripts/mruby/macos.rb
@@ -5,7 +5,7 @@ MRuby::Build.new do |conf|
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
   # C compiler settings
-  conf.cc.defines << 'MRB_INT64'
+  conf.defines << 'MRB_INT64'
   conf.cc.flags += %w[-fpermissive -O2]
 
   conf.enable_cxx_abi
