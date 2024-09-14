35c6dd655f155df0ced66eb6eae4636f74a1b341
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-09-14 11:21:55 -0300

    Nevermind on MSVC I don't have the resources to test for this right now, I think I will end up removing the Windows build if this continues like that

--- scripts/mruby/windows.rb
+++ scripts/mruby/windows.rb
@@ -1,10 +1,12 @@
 MRuby::Build.new do |conf|
-  conf.toolchain :visualcpp
+  conf.toolchain :gcc
 
   # Include gems
   conf.gembox File.expand_path('gosu', File.dirname(__FILE__))
 
+  # C compiler settings
   conf.defines << 'MRB_INT64'
+  conf.cc.flags += %w[-fpermissive -O2]
 
   linker.libraries.push('ws2_32')
 
